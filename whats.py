666586104f109b9d6beb2e018ca86bbdaaeefc8d
Anakinツ <99360831+NicolasYakuzaBr@users.noreply.github.com>
2023-02-15 23:16:44 -0300

    Update whats.py

--- whats.py
+++ whats.py
@@ -1,16 +1,7 @@
 import pywhatkit
 
+pywhatkit.sendwhatmsg( "+DD DDD", "Mensage", "Hour" ,"Minute")
 
-while True:
-    print('WhatsClock')
-    print('Envio de mensagem com temporizador')
-    print('Número deve conter DD e DDD')
-    num = input('Número: ')
-    msg = input('mensagem: ')
-    hora = input('hora: ')
-    minuto = input('minutos: ')
-
-    pywhatkit.sendwhatmsg({num}, {msg}, {hora},{minuto})
 
 
 
